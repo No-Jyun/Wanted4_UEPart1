9c2d03ed7b03b20f47c460e6e6463848c93e9c7a
No-Jyun <hj010410@gmail.com>
2026-04-14 14:59:13 +0900

    11강 내용 진행
    - TMap 실습 진행
    - 커스텀 구조체를 TSet에 사용하기 위한 함수 구현
     - 비교 연산자/GetHashType 함수 오버로딩 구현

--- Source/UEPart1/StudentData.h
+++ Source/UEPart1/StudentData.h
@@ -20,6 +20,17 @@ struct FStudentData
 
 	}
 
+	// TSet에 사용하기 위한 함수 오버로딩
+	bool operator==(const FStudentData& InOther) const
+	{
+		return Name == InOther.Name && Order == InOther.Order;
+	}
+
+	friend FORCEINLINE uint32 GetTypeHash(const FStudentData& InStudentData)
+	{
+		return GetTypeHash(InStudentData.Order);
+	}
+
 	UPROPERTY()
 	FString Name;
 
